049337ae37268df648f36d1c8a496f0d470d9015
Nauel Gómez <ngomez@codiara.com>
2026-06-15 20:10:09 +0200

    Patrón AAA

--- test/unit/smoke.test.js
+++ test/unit/smoke.test.js
@@ -1,9 +1,15 @@
 import { it, expect, describe } from 'vitest';
 
+// Patrón AAA
+// 1. Arrange ??
+// 2. Act
+// 3. Assert
+
 describe('Smoke Test', () => {
 
     it('1 + 1 devuelve 2', () => {
         expect(1+1).toBe(2);
+        expect(2+1).toBe(3);
     })
-    
+
 })
